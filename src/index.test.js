ea26a90d31c1e11946291f60f0b46feaace3af63
tnRaro <deokji53@gmail.com>
2020-10-31 17:04:01 +0900

    Add test case

--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,18 @@
-// TODO
+describe("tnid", () => {
+    const tnid = require("./index");
+
+    test("should be done without error", () => {
+        expect(tnid().length).toBe(20);
+        expect(tnid(4).length).toBe(16);
+        expect(tnid(3).length).toBe(12);
+        expect(tnid(2).length).toBe(8);
+        expect(tnid(1).length).toBe(4);
+        expect(tnid(26).length).toBe(104);
+    });
+
+    test("should be throw error", () => {
+        expect(() => tnid(0)).toThrow();
+        expect(() => tnid(-1)).toThrow();
+        expect(() => tnid(-53)).toThrow();
+    });
+});
